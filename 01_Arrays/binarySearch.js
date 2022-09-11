31d30aac8b9486d4dd53869bcca3bc6feaa92237
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-11 20:37:44 +0530

    1st file

--- 01_Arrays/binarySearch.js
+++ 01_Arrays/binarySearch.js
@@ -1,3 +1,25 @@
 // Binary Search
-// pre-requisite: Binary Search can work on SORTED ARRAY ONLY/-
-function binarySearch(arr, value) {}
+// pre-requisite: Binary Search can work ONLY on SORTED ARRAY
+function binarySearch(arr, value) {
+  // to check for empty array
+  if (arr.length < 1) return null;
+  let start = 0;
+  let end = arr.length - 1;
+  // starting and ending indexes respectively
+
+  let mid; // middle index
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (arr[mid] === value) {
+      console.log(`${value} found at index ${mid}`);
+      return;
+    } else if (arr[mid] < value) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  console.log(`${value} NOT FOUND!!`);
+  return 1;
+}
+binarySearch([4, 32, 46, 78, 91], -78);
