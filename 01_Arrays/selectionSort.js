6bc0dc295ecf3677e413ad8b8c11409eec0331dd
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-11 22:56:20 +0530

    comment added

--- 01_Arrays/selectionSort.js
+++ 01_Arrays/selectionSort.js
@@ -1,31 +1,15 @@
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let exchangeWith = i;
+    let min_index = i;
+    // smallest number in the right side of the array
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[exchangeWith] > arr[j]) {
-        exchangeWith = j;
+      if (arr[min_index] > arr[j]) {
+        min_index = j;
       }
-      /*
-       There can be more than 1 numbers less than current element to find the smallest among those we need to compare it against exchangeWith as it will update the values. 
-       
-       Example [10, 13, 15, 5, 2, 7].
-       If I had used arr[i] > arr[j] it would have made the exchangeWith variable
-       1. arr[exchangeWith] = 5 as 5 < 10
-       2. arr[exchangeWith] = 2 as 2 < 10
-       3. arr[exchangeWith] = 7 as 7 < 10
-       and would have swapped 10 and 7 instead of 10 and 2.
-       
-       So I used arr[exchangewith] > arr[j] as it will update the smallest value we are comparing against.
-       Ex- [10, 13, 15, 5, 2, 7]
-       1. arr[exchangeWith] = 5 as 5 < 10
-       2. arr[exchangeWith] = 2 as 2 < 5
-       3. arr[exchangeWith] = 2 as still 2 < 7
-       Which will result in swapping 10 and 2.
-      */
     }
     // swapping smallest with current
-    let temp = arr[exchangeWith];
-    arr[exchangeWith] = arr[i];
+    let temp = arr[min_index];
+    arr[min_index] = arr[i];
     arr[i] = temp;
   }
   console.log(arr);
