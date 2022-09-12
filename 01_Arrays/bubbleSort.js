6243c275dd54d29c16940b503939d56798f94763
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-13 00:14:49 +0530

    updated bubbleSort

--- 01_Arrays/bubbleSort.js
+++ 01_Arrays/bubbleSort.js
@@ -1,19 +1,42 @@
 // As the name suggests the greatest value will bubble at the end of the array
+/*
+  Pseudocode
+  1. If size of the array is n then it will take exacty n-1 PASSES to sort
+     
+       Ex  arr: [20, 30, 40, 10] n = 4
+        Pass 1: [20, 30, 10, 40]
+        Pass 2: [20, 10, 30, 40]
+        Pass 3: [10, 20, 30, 40]
+     
+     It will always take exactly n-1 (3 passes) to sort the array
+    Therefore the outer for loop runs n-1 times
+  
+  2. Now inner for loop j = 0 and goes till j < arr.length - 1 - passes. BUT WHY ??
+
+     j < arr.length-1 is there because when j = arr.length-1 then j+1 becomes arr.length. This j+1 index is out of bounds.
+     BUT WHY -passes also?
+       
+        Ex arr: [20, 30, 40, 10] n = 4
+        Pass 1: [20, 30, 10,| 40]  1 element is in its correct position
+        Pass 2: [20, 10,| 30, 40]  2 element is in its correct position
+        Pass 3: [10,| 20, 30, 40]  3 element is in its correct position
+        The 1 remaining element is always in its sorted position.
+        
+    j < (arr.length - passes)  because after 1st pass we don't need to check the last element. Similarly after 2nd pass we don't need to check the 2nd to last element and so on...
+
+*/
 function bubbleSort(arr) {
-  let swap = 0;
-  do {
-    // On each pass swap is initialized to 0.
-    swap = 0;
-    for (let i = 0; i < arr.length - 1; i++) {
-      // swap if current element (arr[i]) > next element(arr[i + 1])
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swap++;
+  let passes = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - passes; j++) {
+      if (arr[j + 1] < arr[j]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
     }
-  } while (swap > 0);
+    passes++;
+  }
   console.log(arr);
 }
-bubbleSort([5, 4, 3, 2, 1, 10, 4, 2]);
+bubbleSort([5, 10, 134, 2, 3, 8, 16, 4, 2]);
