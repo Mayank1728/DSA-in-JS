7e8c45291bb1ab54ded3af1d5c00e65b3e136798
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-12 22:44:32 +0530

    selectionSort final added

--- 01_Arrays/selectionSort.js
+++ 01_Arrays/selectionSort.js
@@ -1,17 +1,53 @@
 function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let min_index = i;
-    // smallest number in the right side of the array
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[min_index] > arr[j]) {
-        min_index = j;
+  /*
+    Pseudocode:
+    1. outer for-loop to move the current element on each itration
+    2. inner for-loop to scan the array starting the from current element to the end of the array for minIndex
+    3. now swap current element with minIndex;
+  */
+  // length of the array is n then it will go from 0 to n - 2 cuz last element
+  // is already sorted
+  for (let i = 0; i < arr.length - 1; i++) {
+    let currIndex = i;
+    let minIndex = currIndex;
+    for (let j = currIndex; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
       }
     }
-    // swapping smallest with current
-    let temp = arr[min_index];
-    arr[min_index] = arr[i];
-    arr[i] = temp;
+    // swap currIndex with minIndex
+    // Method 1. [arr[minIndex], arr[currIndex]] = [arr[currIndex], arr[minIndex]]
+    // Method 2. swap using temp, without using temp etc
+    const temp = arr[currIndex];
+    arr[currIndex] = arr[minIndex];
+    arr[minIndex] = temp;
   }
   console.log(arr);
 }
-selectionSort([1, 5, 10, 2, 3, 100, 18, 72]);
+selectionSort([1, 5, 10, 2, 100, 18, 72]);
+/*
+    Time complexity => O(n^2) why ?
+
+    outer loop runs : n times
+    inner loop runs : whatever the value of inner loop is at that time - 1
+    This means total no of comparisons are
+    when
+    i       j runs
+    0       n times
+    1       n - 1 times
+    2       n - 2 times
+    .
+    .
+    .
+    n - 4   n - (n - 4) => 4 times
+    n - 3   n - (n - 3) => 3 times
+    n - 2   n - (n - 2) = > 2 times
+
+    => 2 + 3 + 4 + 5 + .... + (n - 2) + (n - 1) + n
+    => (n(n + 1)/ 2) - 1
+    => ((n^2 + n)/2) - 1
+    => for very very very large values of n we CAN IGNORE CONSTANTS and less dominant terms
+    => n^2
+
+  Space Complexity => O(1) constant space
+*/
