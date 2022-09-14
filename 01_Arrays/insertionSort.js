40c976a48be58505b1acfa8fc07f49c89239f234
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-14 23:31:29 +0530

    pseudocode for insertionSort

---
+++ 01_Arrays/insertionSort.js
@@ -0,0 +1,16 @@
+// Insertion sort
+/*
+ PseudoCode
+ we consider 1 element to be sorted and the right part to be unsorted
+  | shows the partition for Left part and Right part of the array
+  Ex - [5,4,2,3,1]
+  [5|,4,2,3,1]
+  [4,5|,2,3,1]
+  [2,4,5,|3,1]
+  [2,3,4,5|,1]
+  [1,2,3,4,5]
+
+  Time and space complexity
+*/
+
+function insertionSort(arr) {}
