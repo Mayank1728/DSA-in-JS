60a397644d20d5a0b1ce82f3adad183693af3e9a
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-16 23:52:37 +0530

    stings update

--- 10_strings/01_stirngsInJS.js
+++ 10_strings/01_stirngsInJS.js
@@ -4,9 +4,28 @@ shift + alt + a for multiline comment
 Things covered
 1. string as data type
 2. Most important functions associated with strings
+ string.length
+ 
+  .split(' ')
+  .join(' % ')
+  Json.stringify and Json.parse
+  .indexOf and .includes
+  Convert character to ASCII code and vice-versa
+  toUpper() and toLower()
+  How to select character after 2 skips or n number of skips like "hello" -> "hlo
+  slice and splice
+
+
 3. .reverse() is available on arrays only
 4. palindrome both methods for numbers as well as converting strings
 5. "cccooooodddiing" should become "coding" || remove consequetive duplicates
 6. Problem to find out the longest string out of N stringss
 7. 
 */
+/* There are 6 primitive data types in js
+   1. Number -> 5,-10, 3.4, -4 X 10^5 etc numbers
+   2. String -> "a", 'abbccad', 'true', "hello!" etc
+   3. Boolean -> true or false
+   4. NaN -> Not a Number
+   5. undefined -> the variable is undefined
+   6. null -> the variabe is defined but is empty */
