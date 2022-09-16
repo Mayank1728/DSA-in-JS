e00456ed29f905be77ce4b3b0718bda30b2eede4
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-16 17:55:47 +0530

    lets start string manipulations!!

---
+++ 10_strings/01_stirngsInJS.js
@@ -0,0 +1,12 @@
+// string as datatype in js
+/* 
+shift + alt + a for multiline comment
+Things covered
+1. string as data type
+2. Most important functions associated with strings
+3. .reverse() is available on arrays only
+4. palindrome both methods for numbers as well as converting strings
+5. "cccooooodddiing" should become "coding" || remove consequetive duplicates
+6. Problem to find out the longest string out of N stringss
+7. 
+*/
