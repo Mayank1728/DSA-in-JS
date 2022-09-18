2613c057a82f17ccbed1d1ef7b933d89189ec7bb
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-18 19:29:31 +0530

    tc update

--- 08_recursion/04_upToN.js
+++ 08_recursion/04_upToN.js
@@ -2,7 +2,11 @@
 // Eg. for input 4, print 1 2 3 4 and 4 3 2 1
 
 /* 
-   BOTTOM to TOP approach
+   BOTTOM to TOP approach : first printing greater value
+   then samller value
+
+   5 4 3 2 1
+
    We are checking base case then printing value of n
    Then we are recursively calling the function with n - 1
 */
@@ -15,7 +19,11 @@ function printDESC(n) {
 }
 
 /* 
-   TOP to BOTTOM approach
+   TOP to BOTTOM approach: first printing smaller value
+   then greater value
+
+   1 2 3 4 5
+   
    We are checking base case then 
    We are recursively calling the function with n - 1
    Then printing value of n
