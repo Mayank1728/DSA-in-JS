ffee23b28265fafa3d4207f0c19c175bb638ae14
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-18 19:27:43 +0530

    printing numbers upto n and from n to 1

---
+++ 08_recursion/04_upToN.js
@@ -0,0 +1,33 @@
+// printing numbers from and upto N
+// Eg. for input 4, print 1 2 3 4 and 4 3 2 1
+
+/* 
+   BOTTOM to TOP approach
+   We are checking base case then printing value of n
+   Then we are recursively calling the function with n - 1
+*/
+function printDESC(n) {
+  if (n === 0) {
+    return;
+  }
+  console.log(n);
+  printDESC(n - 1);
+}
+
+/* 
+   TOP to BOTTOM approach
+   We are checking base case then 
+   We are recursively calling the function with n - 1
+   Then printing value of n
+*/
+function printASC(n) {
+  if (n === 0) {
+    return;
+  }
+  printASC(n - 1);
+  console.log(n);
+}
+
+printASC(5);
+console.log('');
+printDESC(5);
