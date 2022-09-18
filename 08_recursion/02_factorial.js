be52fd87b81993d2a9f92173c5e6a2b8955a264e
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-18 12:30:38 +0530

    update

--- 08_recursion/02_factorial.js
+++ 08_recursion/02_factorial.js
@@ -1,21 +1,11 @@
-function factorial(n) {
-  if (n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
-console.log(factorial(5));
-
 /*
-  
   What is 5! ?
    5 * 4 * 3 * 2 * 1
          OR
  
   What is 5! ?
    5 * 4!
-   
+
       What is 4! ?
           4 * 3!
 
@@ -36,6 +26,13 @@ console.log(factorial(5));
       returns 4 * 6 = 24
 
     returns 5 * 24 = 120
-                        
-
+                       
  */
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(5));
