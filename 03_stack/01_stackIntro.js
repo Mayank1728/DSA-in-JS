ea3b101d427a03ac63a8251b4b3953c8f884ab83
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-19 22:46:42 +0530

    intro to stack data type

--- 03_stack/01_stackIntro.js
+++ 03_stack/01_stackIntro.js
@@ -1,1 +1,20 @@
-// Intro for js
+/*
+   What is stack ?
+   Stack is a Data Structure which uses LIFO(Last In First Out)
+   principle. You can only insert at the top and remove from the top.
+
+
+   Let's consider a stack of clothes. The cloth you stakced on
+   top is the last cloth. Now When you want a cloth, the cloth 
+   you inserted at last will come out first. 
+   This is LIFO.
+   You might ask yourself how to access my favourite shirt in
+   between the clothes stack ? Thats the NEAT part you can't.
+   You can only insert at the top and remove from the top.
+   
+   Stacks can be implemented with 
+   1. Arrays
+   2. LinkedLists
+
+   I will implement stack with arrays as well as linkedList.
+*/
