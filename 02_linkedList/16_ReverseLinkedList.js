dc082eebf5d457c6f8dd84efb15e9865ee94ca2a
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-20 23:43:08 +0530

    reverse a linkedList

---
+++ 02_linkedList/16_ReverseLinkedList.js
@@ -0,0 +1,155 @@
+/*
+   Reverse a linked List
+   1. if linkedList is empty, return null
+   2. else
+      2.1 while curr exists
+         2.1.1 next = curr.next , prev = null, curr = this.head
+         2.1.2 curr.next in next and curr.next = prev 
+         2.1.3 prev = curr and curr = next;
+
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+// singlyLinked class initialized
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    // length of the linked list
+    this.length = 0;
+  }
+  push(val) {
+    if (this.length === 0) {
+      this.head = new Node(val);
+      this.tail = this.head;
+    } else {
+      let traverse = this.head;
+      // when traverse.next is null this means we
+      // have reached the end of the linkedList
+      // now we need to push new node here
+      while (traverse.next != null) {
+        traverse = traverse.next;
+      }
+      traverse.next = new Node(val);
+      // finally change the tail
+      this.tail = traverse.next;
+    }
+    this.length++;
+  }
+  pop() {
+    if (!this.length) {
+      // will evaluate to true when length = 0
+      console.log('ERROR Empty linkedList!!');
+      return null;
+    }
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      console.log('LinkedList is NOW EMPTY!!');
+    } else {
+      let traverse = this.head;
+      // will break out when reaches 2nd to last node
+      while (traverse.next.next != null) {
+        traverse = traverse.next;
+      }
+      // make the 2nd last node as the last node
+      // 2nd last node becomes new tail
+      traverse.next = null;
+      this.tail = traverse;
+    }
+    // finally decreae the length of the linkedList
+    this.length--;
+  }
+  shift() {
+    if (!this.length) {
+      console.log('ERROR LinkedList is empty!!');
+      return null;
+    }
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      console.log('LinkedList is now EMPTY!');
+    } else {
+      // prevHead stores old head
+      const prevHead = this.head;
+      // new head is equal to next node
+      this.head = this.head.next;
+      // old head .next property is null
+      prevHead.next = null;
+    }
+    this.length--;
+  }
+  unshift(val) {
+    if (!this.length) {
+      this.head = new Node(val);
+      this.tail = this.head;
+    } else {
+      // created new node with provided value
+      let first = new Node(val);
+      // combining new node with head
+      first.next = this.head;
+      // new node is the new head now
+      this.head = first;
+    }
+    this.length++;
+  }
+  search(val) {
+    if (!this.head) {
+      console.log('The linkedList is empty!');
+      return null;
+    } else {
+      let traverse = this.head;
+      let index = 0;
+      while (traverse) {
+        if (traverse.val === val) {
+          console.log(`Found ${val} at ${index}th index`);
+          // to stop further traversal you can also use break
+          return 0;
+        }
+        traverse = traverse.next;
+        index++;
+      }
+      console.log('Value NOT FOUND!!');
+      return null;
+    }
+  }
+  show() {
+    if (!this.head) return null;
+    let traverse = this.head;
+    while (traverse != null) {
+      console.log(traverse.val);
+      traverse = traverse.next;
+    }
+  }
+  reverse() {
+    if (!this.head) return null;
+    this.tail = this.head;
+    let prev = null;
+    let curr = this.head;
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    this.head = prev;
+    this.show();
+    return this.head;
+  }
+}
+
+// playlist is an empty linkedList
+let playList = new SinglyLinkedList();
+playList.push('Eyes blue like the atlantic');
+playList.push('If I killed someone for you');
+playList.push('Change my clothes');
+playList.push("The devil doesn't bargain");
+playList.push('Jesus in LA');
+playList.push('Dopamine Addict');
+playList.reverse();
+console.log(playList.reverse());
---
+++ 03_stack/04_stackReverse.js
@@ -0,0 +1,77 @@
+/* 
+   Reverse a stack
+   
+   Method 1:
+   1.  
+   
+
+*/
+
+class Stack {
+  constructor() {
+    this.data = [];
+    this.length = 0;
+    this.top = -1;
+  }
+  push(val) {
+    this.data[++this.top] = val;
+    this.length++;
+    return this.data;
+  }
+  pop() {
+    if (this.top === -1) {
+      console.log('Stack Underflow');
+      return 'Stack Underflow';
+    } else {
+      this.length--;
+      this.top--;
+      return this.data.pop();
+    }
+  }
+  peek() {
+    if (this.top !== -1) {
+      console.log(this.data[this.top]);
+      return this.data[this.top];
+    }
+    console.log('stack is empty');
+    return false;
+  }
+  show() {
+    for (let i = 0; i <= this.top; i++) {
+      console.log(this.data[i]);
+    }
+  }
+  isEmpty() {
+    if (this.top <= -1) {
+      console.log('The stack is empty');
+      return true;
+    }
+    return false;
+  }
+}
+let books = new Stack();
+books.push('12 Rules for Life by Jordan B. Peterson');
+books.push('War of Art by Steven Pressfield');
+books.push('How to fail at almost everything & still win big');
+books.push('Richest Man in babyloan');
+books.push('48 Laws of power');
+books.push('Sapiens A brief history of humankind');
+books.push('Guns Germs and Steel');
+books.push('Predictably irrational');
+books.push('Extreme ownership');
+books.push('The selfish gene');
+books.show();
+console.log();
+let temp1 = new Stack();
+while (books.length) {
+  temp1.push(books.pop());
+}
+let temp2 = new Stack();
+while (temp1.length) {
+  temp2.push(temp1.pop());
+}
+console.log();
+while (temp2.length) {
+  books.push(temp2.pop());
+}
+books.show();
