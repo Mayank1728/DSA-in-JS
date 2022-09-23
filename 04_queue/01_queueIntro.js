7acc073a112e24e13018836378356bf252e845a2
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-23 16:44:09 +0530

    intro to queue

---
+++ 04_queue/01_queueIntro.js
@@ -0,0 +1,18 @@
+/*
+  What is Queue ?
+  
+  What is the first thing that comes to your mind when
+  you hear the word queue ? 
+  Queue at the airport, bookshop, queue for movie tickets...
+  In a queue, the first person to move in is the first person to move out.
+  
+  Similarly Queue data structure works on the principle
+  of FIFO(First In First Out).
+
+  In stacks we push & pop elements from the same end.
+  However, in a queue we push elements to rear and pop elements from
+  the top/front. 
+
+  Time Complexity: O(1)
+
+*/
