1ff1ffcfdc1d3358e0dd3e513b1d4a50d2fd5a6f
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-25 00:45:22 +0530

    method name changes implemented

--- 04_queue/02_implementingQueue.js
+++ 04_queue/02_implementingQueue.js
@@ -1,12 +1,15 @@
 /*
    Queue Class implementation
-   Technically we should not implement queue using array
+
+   Technically we should not implement queue using array as the dequeue opeartion takes O(n) time to implement under the hood.
+   So queue should be implemented using LinkedList.
+
    To avoid confusion I used enqueue and dequeue
-   1. push function:
+   1. enqueue function:
      1.1 if the queue is empty, Update the top and rear
      1.2 update rear = val
-     1.3 push the value in this.data
-   2. pop function
+     1.3.enqueue the value in this.data
+   2. dequeue function
      2.1 if the queue is empty, return false
      2.2 if len of queue is 1, top = null and rear = null
      2.3 else update top
@@ -21,37 +24,37 @@ class Queue {
     this.length = 0;
   }
   // adding value at the end of the array
-  push(val) {
+  enqueue(val) {
     // if the queue was EMPTY, update the top = val
     if (!this.length) {
       this.top = val;
     }
 
-    // push the value, update the rear, update the len
-    this.data.push(val);
+    //.enqueue the value, update the rear, update the len
+    this.data.enqueue(val);
     this.rear = val;
     this.length++;
     return true;
   }
   // removing value from the start of the array
-  shift() {
+  dequeue() {
     // if top is empty, return false
     if (!this.top) {
       console.log('Empty QUEUE!');
       return false;
     }
 
-    // if len === 1, then the queue will be empty after shift()
-    // so top = null , rear = null and data.shift()
+    // if len === 1, then the queue will be empty after dequeue()
+    // so top = null , rear = null and data.dequeue()
     if (this.length === 1) {
       this.top = null;
       this.rear = null;
-      this.data.shift();
+      this.data.dequeue();
       return true;
     }
 
-    // shift the data and reduce the length and update rear
-    this.data.shift();
+    // dequeue the data and reduce the length and update rear
+    this.data.dequeue();
     this.length--;
     this.rear = this.data[this.length - 1];
   }
@@ -62,14 +65,14 @@ class Queue {
   }
 }
 let movie = new Queue();
-movie.push(1);
-movie.push(2);
-movie.push(3);
-movie.push(1);
-movie.shift();
-movie.push(2);
-movie.push(3);
-movie.shift();
-movie.push(5);
+movie.enqueue(1);
+movie.enqueue(2);
+movie.enqueue(3);
+movie.enqueue(1);
+movie.dequeue();
+movie.enqueue(2);
+movie.enqueue(3);
+movie.dequeue();
+movie.enqueue(5);
 movie.show();
 console.log(movie.top, movie.length);
