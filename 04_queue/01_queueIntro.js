3017b1e5e1056fd6a7e67698eb5d867484fa4fe3
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-25 16:36:30 +0530

    minor changes

--- 04_queue/01_queueIntro.js
+++ 04_queue/01_queueIntro.js
@@ -6,17 +6,15 @@
   In a queue, the first person to move in is the first person to move out.
   
   Similarly Queue data structure works on the principle
-  of FIFO(First In First Out).
+  of FIFO(First In First Out). In stacks we push & pop elements from the same
+  end. However, in a queue we push elements to rear and pop elements from
+  the top/front. 
 
   Real life examples of Queue
   1. while listening to Songs, you click "add songs to the queue". The songs
       added first to the queue are played first.
   2. Queue at the theatre, person who came in first came out first.
   
-
-  In stacks we push & pop elements from the same end.
-  However, in a queue we push elements to rear and pop elements from
-  the top/front. 
 
   Time Complexity: Insertion O(1) and Deletion O(1)
   Space Complexity: O(n) 
