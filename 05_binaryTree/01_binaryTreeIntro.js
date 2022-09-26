a344f2db8a9c5f1b8c1fd16bc6511bf2d2052197
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-09-27 00:31:13 +0530

    updates

--- 05_binaryTree/01_binaryTreeIntro.js
+++ 05_binaryTree/01_binaryTreeIntro.js
@@ -1,10 +1,35 @@
 /*
            Binary Trees
    
-   Similar to the family tree, a binary Tree is represented by
-    1. root node (AKA parent node)
-    2. left child
-    3. right child
-    Both child nodes AKA leaf nodes.
+    1. Difference between linear and non linear data structure
+
+        Linear data structure like arrays, linkedList, stack and queues are data
+        is stored in SEQUENTIAL manner (one after the another)
+        
+        Trees are non-linear data structure which means data is stored NON-SEQUENTIALLY.
+        Trees contains levels.
+    
+    2. A tree contains ONLY 1 ROOT NODE. A node can reference(point towards : ->)
+        ONLY to children nodes. Furthermore, a node CANNOT reference(point towards : ->)
+        back to parent node or to its sibling node. 
+
+    
+    Terminology:
+        root node: first/top most node of a tree
+        leaf: node with no children
+        sibling: nodes with same parent
+        edge: connection b/w 2 nodes
+
+
+    Real World Examples:
+    1. files directory
+    2. html dom
+    3. AI (ex- minimax tree): playing against itself what happens next
+    4. JSON: javascript object notation
+
+    Types of trees
+    1. binary tree
+    2. binary Search tree
+    3. AVL etc
 
 */
