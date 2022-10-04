42bdf05de153d97821b69f7a95a67043f46b5322
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-10-04 15:12:45 +0530

    updates

--- 02_linkedList/01_whyLinkedList.js
+++ 02_linkedList/01_whyLinkedList.js
@@ -3,18 +3,28 @@
 
   Problem with traditional ARRAYS
 
-    1. arrays are of fixed size(in c and c++). So when 
+    1. arrays are of fixed size(in c and c++). So when an
         ARRAY IS COMPLETELY FILLED BUT WE STILL want to ADD ELEMENT to it
-          1.1 create new array with prev array's size + 1
-          1.2 copy all the contents of the previous array into the newer array
-          1.3 then add element to the newer array
+        Under the hood what happens in C++ is it
+          1.1 creates a new array, TWICE THE SIZE of the previous one.
+          1.2 copies all the contents of the previous array into the newer array
+          1.3 and then adds the element to the newer array
     
     2. Similar problem arises when we want to delete an element
+        When you delete an element from in between the array, we need to
+        shift elements after the deleted element by -1 place.
     
-    This insertion and deletion problem costs us Time: O(n) and space: O(n);
 
-    Therefore, we use linkedlist because it offers O(1) time complexity
-    for insertion and deletion.
+    COST of INSERTION: O(n) 
+          Worst case when the array is full and I have to insert an element
+    
+
+    COST of DELETION: O(n) 
+          Worst case when I delete the first element, and now we need to shift all the              elements to the left.
+
+    Therefore, we use linkedlist because it offers
+          O(1) Insertion
+          O(1) Deletion (when you remember the tail node)
 
     TRADEOFFS:
       1. Arrays have very fast lookup time and random access of elements(you can
