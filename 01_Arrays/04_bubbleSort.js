2d32d13fc0ff62952a2ff414ba14cbee8b2646b0
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-10-06 15:08:32 +0530

    updates

--- 01_Arrays/04_bubbleSort.js
+++ 01_Arrays/04_bubbleSort.js
@@ -1,44 +1,68 @@
 // As the name suggests the greatest value will bubble at the end of the array
 /*
-  Pseudocode
-  1. If size of the array is n then it will take exacty n-1 PASSES to sort
-     
-       Ex  arr: [20, 30, 40, 10] n = 4
-        Pass 1: [20, 30, 10, 40]
-        Pass 2: [20, 10, 30, 40]
-        Pass 3: [10, 20, 30, 40]
-     
-     It will always take exactly n-1 pases(3 passes in above example) to sort the array
-    Therefore the outer for loop runs n-1 times
-  
-  2. Now inner for loop j = 0 and goes till j < arr.length - 1 - passes. BUT WHY ??
+    3 Things to consider in bubble sort
 
-     j < arr.length-1 is there because when j = arr.length-1 then j+1 becomes arr.length. This j+1 index is out of bounds.
-     BUT WHY -passes also?
-       
-        Ex arr: [20, 30, 40, 10] n = 4
-        Pass 1: [20, 30, 10,| 40]  1 element is in its correct position
-        Pass 2: [20, 10,| 30, 40]  2 element is in its correct position
-        Pass 3: [10,| 20, 30, 40]  3 element is in its correct position
-        The 1 remaining element is always in its sorted position.
+        1. Swap Variable
+            We Know that if Swaps REMAIN 0, the array is sorted
+            Else it needs to be sorted
         
-    j < (arr.length - passes)  because after 1st pass we don't need to check the last element.
-    Similarly after 2nd pass we don't need to check the 2nd to last element and so on...
+        2. Passes Variable
+            On Each passes the largest value will bubble at the end
+            Ex - Array : [40, 30, 20, 10]
+                Pass 1 : [30, 20, 10, 40] 40 is at correct position
+                Pass 2 : [20, 10, 30, 40] now 30, 40 are at correct position
+                Pass 3 : [10, 20, 30, 40] now 20, 30, 40 at correct position
+                ONLY 1 element is left so No need to check 10
+
+        MOST IMPORTANT PART this optimizes our code
+
+        3. Why does the inner for-loop run (curr < arr.length - passes - 1) times ?
+            3.1 WHY arr.length - 1 ?
+                Because we are comparing curr and next element. If we run the
+                loop till curr < arr.length the PROBLEM ARISES ON LAST PASS:
+                  curr = arr.length - 1 (curr is on last index)
+                  next will be curr + 1
+                  this means next = arr.length
+                  BUT the index is from 0 to arr.length - 1
+                  This means next will go out of bounds
+                Therefore we run curr < arr.length - 1 so that next will be
+                at max equal to arr.length - 1;
+            
+            3.2 WHY arr.length - 1 - passes? 
+                  we subtract passes because after each pass the element at the
+                  last is sorted.
+                  Ex - Array : [40, 30, 20, 10] pass = 0 will run till end
+                  Pass 1 : [30, 20, 10, 40] pass = 1
+                            This means 40 is at correct pos no need to check 40
+                  Pass 2 : [20, 10, 30, 40] pass = 2
+                            This means last 2 elements (30, 40) are sorted
+                            So no need to check 30 and 40
+                  Pass 3 : [10, 20, 30, 40] similarly pass = 3
+                            This means last 3 elements (20, 30, 40) are sorted
+                  ONLY 1 element is left so No need to check 10 
+
 
 */
 function bubbleSort(arr) {
+  let swap = 0;
   let passes = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1 - passes; j++) {
-      if (arr[j + 1] < arr[j]) {
-        let temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
+  do {
+    swap = 0;
+    for (let curr = 0; curr < arr.length - passes - 1; curr++) {
+      let next = curr + 1;
+      if (arr[curr] > arr[next]) {
+        let temp = arr[curr];
+        arr[curr] = arr[next];
+        arr[next] = temp;
+        swap++;
       }
     }
     passes++;
-  }
+  } while (swap != 0);
   console.log(arr);
 }
 bubbleSort([5, 10, 134, 2, 3, 8, 16, 4, 2]);
 // Time complexity: O(n^2)
+/*
+    Why O(n^2) ?
+*/
