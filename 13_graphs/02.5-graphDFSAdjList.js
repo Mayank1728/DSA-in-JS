b572578c6b9dad20a6eba7bbb40b945183a1843a
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2022-11-08 01:25:35 +0530

    iterative implemented

--- 13_graphs/02.5-graphDFSAdjList.js
+++ 13_graphs/02.5-graphDFSAdjList.js
@@ -4,14 +4,14 @@
   There are 2 ways of implementing DFS
    1. Recursive
    2. Iterative (using stack)
-
+   Todo:  debug it once more!
 */
 const adjList = {
   a: ['b', 'c'],
   b: ['d'],
   c: ['e'],
   d: [],
-  e: ['b', 'a'],
+  e: ['b', 'f'],
   f: ['d'],
 };
 // let adjList = {
@@ -23,10 +23,12 @@ const adjList = {
 //   F: ['C', 'E'],
 // };
 
+// why we don't have a base case here ?
 // 1. Recursive Approach
 function dfs(adjList, start, map = {}) {
   // first mark start as visited
   map[start] = true;
+  console.log(start);
   // Now loop through each neighbour of start
   for (let neighbour of adjList[start]) {
     // if Neighbour is NOT yet visited
@@ -37,8 +39,26 @@ function dfs(adjList, start, map = {}) {
   }
   // reching here means its a dead end or leaf node
   // you can't go deeper so print the value of start.
-  console.log(start);
 }
-dfs(adjList, 'A');
+dfs(adjList, 'a');
 
 // 2. Iterative approach using stack
+function dfsIterative(adjList, source = 'a') {
+  let stack = [source];
+  let map = {};
+  map[source] = true;
+  while (stack.length > 0) {
+    let currNode = stack.pop();
+    console.log(currNode);
+    for (neighbour of adjList[currNode]) {
+      if (!map[neighbour]) {
+        map[neighbour] = true;
+        stack.push(neighbour);
+      }
+    }
+  }
+}
+dfsIterative(adjList);
+// Time : O(V + E) why ? don't know exactly
+// Space : O(V) lets consier a grap have V nodes connected like linkedList
+// so at worst there will be V call frames at same time so O(V) space
