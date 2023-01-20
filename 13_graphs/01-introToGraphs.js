9b392f2e854b2fd3d02460d79c7aea90b490431f
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2023-01-20 17:16:49 +0530

    updates

--- 13_graphs/01-introToGraphs.js
+++ 13_graphs/01-introToGraphs.js
@@ -1,7 +1,27 @@
 /* 
    Trees and linkedList are a subset of Graphs.
-   Graphs have nodes or more formally known as Vertices and
-   Edges which shows the connection between 2 Vertices
+   G = (V, E)
+   Graph is a collection of Vertices(Nodes) and
+   Edges. Edges show the connection between 2 Vertices
+
+   Practical Applications of Graphs
+    1. Networks :
+        1.1 Netural Networks
+        1.2 Social Networks like Facbook, Instagram
+        1.3 Railway Networks etc.
+    2. Search engine crawlers
+    3. GPS
+    4. Movie recommendations : Netflix, IMDB etc
+    5. Path finding Algorithms: Uber, Lyft, Maps etc.
+    
+    Self Loop : When an edge starts and ends at the same node without
+               visiting any other node.
+
+
+    Degree of a Vertex: No of nodes Directly Adjacent or Connected to it.
+
+    # No of edges = 0.5 X (Sum of the degrees of all Nodes)
+
 
    Graphs can be represented in 3 ways :
      1. Adjacency List
@@ -15,6 +35,10 @@
      4. Unweighted Graph : All paths are of equal length
 
    No of edges <= (Vertices)^2
+
+   Max no of edges in Undirected graph = 0.5 X N X (N - 1)
+   Max no of edges in Directed graph =   N X (N - 1)
+   *N represents total number of nodes and Self loop is considered as an edge.
    
 */
 
@@ -30,10 +54,11 @@ const grid = [
   [0, 1, 0, 0],
 ];
 
-// matrix[1] represents the COMPLETE ROW => [1, 0, 1]
-// matrix[1][3] represents the ELEMENT AT (row, col) : (1, 3) => 1
+// grid[1] represents the COMPLETE ROW => [1, 1, 0, 0]
+// grid[1][3] represents the ELEMENT AT (row, col) : (1, 3) => 0
 // Here 0 represents free and 1 represents blocked
-// starting from the grid[0][0] you can move along all the zeroes and can't move to 1
+// starting from the grid[0][0] you can move along all the zeroes but
+// can't move towards 1
 // matrix[r][c] = 0 represent NODES
 // matrix[r][c] = 1 represents empty
 
@@ -45,10 +70,11 @@ const adjMatrix = [
   [0, 1, 0, 0],
 ];
 
-// Here adjMatrix[r][c] represents whether there exists an Edge between Vertex r to c
-// Similarly adjMatrix[c][r] represents whether Edge exists between Vertex c to r
+// Here adjMatrix[A][B] represents whether there exists an Edge between Vertex A to B
+// Similarly adjMatrix[B][A] represents whether Edge exists between Vertex B to A
 // 0 implies Edge Doesn't exist
 // 1 implies Edge EXISTS
+// You can think of rows as Source and column as Destination.
 // Space : O(V^2) here V is number of vertices
 
 // 3. Adjacency List
