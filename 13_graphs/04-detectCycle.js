999f53a8b668062a80a1a8a9d428a09df2a99d39
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2023-01-27 00:36:44 +0530

    detect cycle

--- 13_graphs/04-detectCycle.js
+++ 13_graphs/04-detectCycle.js
@@ -5,29 +5,39 @@
    TechieDelight good website for DSA questions
 */
 const adjList = {
-  a: ['b'],
-  b: ['c'],
-  c: [],
+  a: ['b', 'c'],
+  b: ['d'],
+  c: ['e'],
+  d: [],
+  e: ['b', 'f'],
+  f: ['d'],
 };
 
 // quite similar to dfs
 function detectCycle(adjList) {
-  let stack = ['a'];
+  const vertex = Object.keys(adjList);
   let isCyclic = false;
   let discovered = {};
-  while (stack.length) {
-    let curr = stack.pop();
-    if (!discovered[curr]) {
-      discovered[curr] = true;
-      for (let i = adjList[curr].length - 1; i > -1; i--) {
-        stack.push(adjList[curr][i]);
-      }
-    } else {
-      isCyclic = true;
-      break;
+  for (let i = 0; i < vertex.length; i++) {
+    if (!discovered[vertex[i]] && !isCyclic) {
+      isCyclic = dfs(vertex[i], adjList, discovered);
     }
   }
   console.log(isCyclic);
   return isCyclic;
 }
+
+function dfs(vertex, adjList, discovered, isCyclic) {
+  let stack = [vertex];
+  while (stack.length) {
+    let curr = stack.pop();
+    if (!discovered[curr]) {
+      discovered[curr] = true;
+      for (let i = adjList[curr].length - 1; i > -1; i--)
+        stack.push(adjList[curr][i]);
+    } else return true;
+  }
+  return false;
+}
+
 detectCycle(adjList);
