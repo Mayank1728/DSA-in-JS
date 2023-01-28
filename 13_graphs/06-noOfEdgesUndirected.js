90dc7184bb4a33a5dac8e2315c63fdcf4b8c3a66
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2023-01-28 18:21:16 +0530

    done

---
+++ 13_graphs/06-noOfEdgesUndirected.js
@@ -0,0 +1,21 @@
+// Count the no of edges in an undirected graph
+
+const adjList = {
+  a: ['b', 'c'],
+  b: ['a', 'd', 'e'],
+  c: ['a', 'e'],
+  d: ['b', 'f'],
+  e: ['b', 'c', 'f'],
+  f: ['d', 'e'],
+};
+
+function noOfEdges(adjList) {
+  let vertices = Object.keys(adjList);
+  let directedEdges = 0;
+  for (let vertex of vertices) {
+    directedEdges += adjList[vertex].length;
+  }
+  console.log(directedEdges / 2);
+  return directedEdges / 2;
+}
+noOfEdges(adjList);
