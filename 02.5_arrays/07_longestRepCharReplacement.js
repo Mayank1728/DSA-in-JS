90910d36f0c7e6c3449ed2a1869acae2cf17228f
Mayank Mudgal <71082230+Mayank1728@users.noreply.github.com>
2023-03-20 10:44:43 +0530

    O(N) Time and O(1) space

--- 02.5_arrays/07_longestRepCharReplacement.js
+++ 02.5_arrays/07_longestRepCharReplacement.js
@@ -2,6 +2,38 @@
 // return len of longest substring containing same letter
 // after making at most k changes
 
+//Sliding Window Approach
+function characterReplacement(s, k){
+  let start = 0;
+  let map = {};
+  let maxLen = 0;
+  for(let end = 0; end < s.length; end++){
+    // adding curr alphabet to map
+    if(!map[s[end])
+      map[s[end]] = 1;
+    else
+      map[s[end]] += 1;
+    
+    // finding out which alphabet occurs the most
+    let maxFreq = 0;
+    for(let key in map)
+      maxFreq = Math.max(maxFreq, map[key]);
+    
+    // we can also use if statement instead of while loop here
+    // if the currWindow which is (end - start + 1)
+    // minus the maxFreq is greater than k
+    // this means we cannot replace that much characters
+    while(end - start + 1 - maxFreq > k)
+      map[s[start++]] -= 1;
+    
+    // otherwise return  maxLen
+    maxLen = Math.max(maxLen, end - start + 1)
+  }
+  return maxLen;
+  // Time : O(N)
+  // Space : O(26) 26 alphabets max so const space
+}
+
 // BruteForce O(N^2)
 function CharacterReplacement(s, k) {
   let maxLen = 0;
