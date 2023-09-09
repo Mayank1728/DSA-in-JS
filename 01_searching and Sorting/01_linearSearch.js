eb0e03ec861b32719051aff04c2e8bdcf5b5e0a0
Mayank Mudgal <71082230+Mayank1728@users.noreply.github.com>
2023-09-09 19:03:02 +0530

    Update 01_linearSearch.js

--- 01_searching and Sorting/01_linearSearch.js
+++ 01_searching and Sorting/01_linearSearch.js
@@ -2,7 +2,7 @@
 /*
   Linear search searches for a value one-by-one scanning the complete array.
     1. if value is found => shows the index and stops searching.
-    2. if value is NOT FOUND in the complete array => return 1. 
+    2. if value is NOT FOUND in the complete array => return -1. 
     
 */
 function linearSearch(arr, value) {
@@ -15,16 +15,20 @@ function linearSearch(arr, value) {
     }
   }
   console.log(`${value} NOT found`);
-  return 1;
+  return -1;
 }
 linearSearch([5, 6, 10, 1, 2, 3, 4], 4);
 linearSearch(['mars', 'venus', 'earth', 'mercury', 'jupiter'], 'neptune');
 
-// Time Complexity -> O(n) : How does the TIME grows as the size of input array is increased ?
-// Space Complexity -> O(n) : How does the SPACE grows as the size of input array is increased ?
+// Time Complexity -> O(n)
+// Time complexity means How does the TIME grows as the size of input array is increased ?
+// Space Complexity -> O(1) : 
+// Space complexity means How does the SPACE grows as the size of input array is increased ?
 
 /*
-  Do you know?
-
-   In js, arrays & objects are passed by reference and everything else is passed by value. This means when you pass an array/object inside a function, the modification is done on the actual array/object, however, when you pass anything else- string, Number etc data types- values are copied, then modification is done on the copied values not on the original values.
+  NOTE:
+   In js, arrays & objects are passed by reference and everything else is passed by value.
+   This means when you pass an array/object inside a function, the modification is done on the actual array/object, 
+   however, when you pass anything else- string, Number etc data types- values are copied, then modification is done 
+   on the copied values not on the original values.
 */
